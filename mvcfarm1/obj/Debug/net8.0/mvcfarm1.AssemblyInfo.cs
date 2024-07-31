5aa65d61118b302346d0289d66c316214ff23699
Ernesto Garcia Valenzuela <0322103721@ut-tijuana.edu.mx>
2024-07-30 19:44:28 -0700

    aaaaaaaaaaaaaaaaaaaaa

--- mvcfarm1/obj/Debug/net8.0/mvcfarm1.AssemblyInfo.cs
+++ mvcfarm1/obj/Debug/net8.0/mvcfarm1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvcfarm1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4bbc90c818fe043987b0a2df901290b8b24ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56745632d7c5baafda71914c7ba4f7cbca0b21c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvcfarm1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvcfarm1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
