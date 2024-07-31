e2c613bd4a4d587953a32db915f1da06255b75a2
Ernesto Garcia Valenzuela <0322103721@ut-tijuana.edu.mx>
2024-07-30 19:01:37 -0700

    WEB WEB WEB
    Alerta mundial

---
+++ mvcfarm1/Data/mvcfarmDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcfarm1.Models;
+
+namespace mvcfarm1.Data
+{
+    public class mvcfarmDBContext: DbContext
+    {
+        public mvcfarmDBContext(DbContextOptions<mvcfarmDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Company { get; set; }
+        public DbSet<Package> Package { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Company>().ToTable("Company");
+            modelBuilder.Entity<Package>().ToTable("Package");
+            
+        }
+    }
+
+}
---
+++ mvcfarm1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcfarm1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
